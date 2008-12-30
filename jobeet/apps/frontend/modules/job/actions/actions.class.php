a7db07812850c9feb1d2d83ecee6b18ec58fa429
michele.orselli <michele.orselli@5178b5da-d41a-11dd-918e-25ffe7a15323>
2008-12-30 12:14:56 +0000

    day 5: introduction to routing framework

--- jobeet/apps/frontend/modules/job/actions/actions.class.php
+++ jobeet/apps/frontend/modules/job/actions/actions.class.php
@@ -17,8 +17,7 @@ class jobActions extends sfActions
 
   public function executeShow(sfWebRequest $request)
   {
-    $this->jobeet_job = JobeetJobPeer::retrieveByPk($request->getParameter('id'));
-    $this->forward404Unless($this->jobeet_job);
+      $this->job = $this->getRoute()->getObject();
   }
 
   public function executeNew(sfWebRequest $request)
